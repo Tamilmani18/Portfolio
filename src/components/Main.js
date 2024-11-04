6f25257f7646363bf4bd5da426c92421b575f020
Tamil <Tamilmani.tm018@gmail.com>
2024-11-04 22:47:38 +0530

    modified resume

--- src/components/Main.js
+++ src/components/Main.js
@@ -233,7 +233,7 @@ const Main = () => {
             <Contact
               click={+click}
               target="_blank"
-              href="https://drive.google.com/file/d/1J-qNSuAajEdSPojkngRQPVcFenBpV3bj/view?usp=sharing"
+              href="https://drive.google.com/file/d/1F55AL4-DqDK2bdc1NLiRwm0rR5CE8nQ8/view?usp=sharing"
             >
               <motion.h2
                 initial={{
@@ -254,7 +254,7 @@ const Main = () => {
             <Contact
               click={+false}
               target="_blank"
-              href="https://drive.google.com/file/d/1J-qNSuAajEdSPojkngRQPVcFenBpV3bj/view?usp=sharing"
+              href="https://drive.google.com/file/d/1F55AL4-DqDK2bdc1NLiRwm0rR5CE8nQ8/view?usp=sharing"
             >
               <motion.h2
                 initial={{
