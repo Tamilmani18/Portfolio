929e6c202cb43618d2660e398bc3300f793c6f93
Tamil <Tamilmani.tm018@gmail.com>
2025-02-15 23:57:06 +0530

    added certificates

--- src/data/BlogData.js
+++ src/data/BlogData.js
@@ -7,6 +7,7 @@ export const Blogs = [
     imgSrc: "https://leetcard.jacoblin.cool/tamilmani18?ext=contest",
     link: "https://leetcode.com/u/Tamilmani18/",
   },
+  // 
   {
     id: 2,
     name: "The Complete Flutter Development Bootcamp with Dart",
@@ -27,6 +28,14 @@ export const Blogs = [
   },
   {
     id: 4,
+    name: "Problem Solving HackerRank",
+    tags: ["logic-building", "problem-solving"],
+    date: "14 Feb, 2025",
+    imgSrc: "https://res.cloudinary.com/dvuyywuw5/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1739643704/Certificates/Problem_Solving_Basic_zs7ei9.png",
+    link: "https://www.hackerrank.com/certificates/iframe/afba1ba09566/",
+  },
+  {
+    id: 5,
     name: "Introduction to MERN Stack",
     tags: ["mern", "full-stack", "web-app"],
     date: "04 Nov, 2024",
@@ -36,7 +45,7 @@ export const Blogs = [
     // https://simpli-web.app.link/e/jKOgEIkRfOb
   },
   {
-    id: 5,
+    id: 6,
     name: "Android Bug Bounty",
     tags: ["bugs", "error", "android"],
     date: "30 Jul, 2024",
@@ -45,7 +54,7 @@ export const Blogs = [
     link: "https://codered.eccouncil.org/certificate/ee0ce57d-4be2-4ed2-8a19-1d832dfd70a3",
   },
   {
-    id: 6,
+    id: 7,
     name: "Web-Scraping and App development",
     tags: ["workshop", "information-extraction"],
     date: "10 Feb, 2023",
@@ -54,7 +63,7 @@ export const Blogs = [
     link: "https://res.cloudinary.com/dvuyywuw5/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1722576317/Certificates/Web_scrap_and_app_development_i3tlx8.jpg",
   },
   {
-    id: 7,
+    id: 8,
     name: "Machine Learning",
     tags: ["mathworks", "python"],
     date: "11 May, 2024",
@@ -63,7 +72,7 @@ export const Blogs = [
     link: "https://matlabacademy.mathworks.com/progress/share/certificate.html?id=14ee4de1-c422-4498-af7d-147d2e72325a&",
   },
   {
-    id: 8,
+    id: 9,
     name: "Data Visualization with Python",
     tags: ["coursera", "python", "visualization"],
     date: "24 Sep, 2023",
@@ -72,7 +81,7 @@ export const Blogs = [
     link: "https://www.coursera.org/account/accomplishments/verify/NKT3YRUXV98R?utm_source=link&utm_medium=certificate&utm_content=cert_image&utm_campaign=sharing_cta&utm_product=course",
   },
   {
-    id: 9,
+    id: 10,
     name: "Data Visualization with Tableau",
     tags: ["coursera", "tableau", "visualization"],
     date: "26 Sep, 2023",
@@ -81,7 +90,7 @@ export const Blogs = [
     link: "https://www.coursera.org/account/accomplishments/specialization/SFX2PAUX4HL8?utm_source=link&utm_medium=certificate&utm_content=cert_image&utm_campaign=sharing_cta&utm_product=s12n",
   },
   {
-    id: 10,
+    id: 11,
     name: "Engineering Graphics and Design",
     tags: ["edutech", "autocad","3d-models"],
     date: "10 Mar, 2023",
@@ -90,7 +99,7 @@ export const Blogs = [
     link: "https://iggnitewebcertification.lntedutech.com/?certificationID=6tlao4_7p1k0hdx_985376988836103&name=TAMILMANI%20P&course=Engineering%20Graphics%20and%20Design",
   },
   {
-    id: 11,
+    id: 12,
     name: "IT Primer",
     tags: ["edutech", "it-industry"],
     date: "05 Jul, 2021",
@@ -99,7 +108,7 @@ export const Blogs = [
     link: "https://iggnitewebcertification.lntedutech.com/?certificationID=6tlao4_P0ZxbX8k_944101790790462&name=TAMILMANI%20P&course=IT%20Primer",
   },
   {
-    id: 12,
+    id: 13,
     name: "A Professional's Approach to Law and Ethics",
     tags: ["edutech", "law", "ethics"],
     date: "22 Jul, 2023",
